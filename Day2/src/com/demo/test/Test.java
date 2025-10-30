ae6a97fcef4f5548bf86f41fe8be701aa54d5e81
mukund <autimukund9@gmail.com>
2025-10-30 17:01:06 +0530

    dsa

--- Day2/src/com/demo/test/Test.java
+++ Day2/src/com/demo/test/Test.java
@@ -1,7 +1,30 @@
 package com.demo.test;
+
+import java.util.stream.IntStream;
+
 import com.demo.service.*;
+
 public class Test {
-int [][]arr = new int[3][3];
-ArrayService op = new ArrayService();
-op.acceptData();
+
+	public static void main(String[] args) {
+		ArrayService ob = new ArrayService();
+		System.out.println("Enter array Elements:");
+		ob.acceptData();
+		ob.displayData();
+		int[] temp = ob.findSumRowWise();
+		System.out.println("Addition Row Wise:");
+		IntStream.of(temp).forEach(e -> System.out.print(e + ","));
+		System.out.println("");
+		temp = ob.findSumColumnWise();
+		System.out.println("Addition Column Wise:");
+		IntStream.of(temp).forEach(e -> System.out.print(e + ","));
+		System.out.println();
+		ob.rowRotation(false, 1);
+		ob.displayData();
+		System.out.println();
+		ob.columnRotation(false, 1);
+		ob.displayData();
+
+	}
+
 }

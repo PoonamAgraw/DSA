30338529bb12a5c89c5d384170e57c5e78d177dc
mukund <autimukund9@gmail.com>
2025-11-03 14:21:55 +0530

    dsa

--- Day2/src/com/demo/test/Test.java
+++ Day2/src/com/demo/test/Test.java
@@ -24,7 +24,28 @@ public class Test {
 		System.out.println();
 		ob.columnRotation(false, 1);
 		ob.displayData();
-
+		int[][] arr=ob.transpose();
+		displayarray1(arr);
+		if(ob.isSysmmetric()) {
+			System.out.println("symmetric matrix");
+		}else {
+			System.out.println("not symmetric matrix");
+		}
+		
+		if(ob.isIdentity()) {
+			System.out.println("Identity matrix");
+		}else {
+			System.out.println("not Identity matrix");}
+		}
+		
+		private static void displayarray1(int[][] arr) {
+			for(int i=0;i<arr.length;i++) {
+				for(int j=0;j<arr[0].length;j++) {
+					System.out.print(arr[i][j]+"\t");
+				}
+				System.out.println();
+			}
+		
 	}
 
 }

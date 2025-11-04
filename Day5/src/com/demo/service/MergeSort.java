d80a9280fa7f1c30b0bf870b899f7b53e6b56e93
mukund <autimukund9@gmail.com>
2025-11-04 17:11:18 +0530

    dsa

--- Day5/src/com/demo/service/MergeSort.java
+++ Day5/src/com/demo/service/MergeSort.java
@@ -1,5 +1,29 @@
 package com.demo.service;
 
 public class MergeSort {
-
+public static void mergeSort(int [] arr, int start,int mid,int end) {
+	int n1 = mid-start-1;
+	int n2 = end-mid;
+	int [] leftarray = new int[n1];
+	int [] rigtharray = new int[n2];
+	for(int i=0;i<n1;i++) {
+		leftarray[i] = arr[start+i];
+	}
+	for(int i=0;i<n2;i++) {
+		rigtharray[i] = arr[mid+1+i];
+	}
+	int i=0,j=0,k=start;
+	while(i<n1 && j<n2) {
+		if(leftarray[i] < rigtharray[i]) {
+			arr[k] = leftarray[i];
+			i++;
+			k++;
+		}
+		else {
+			arr[k] = rigtharray[j];
+			j++;
+			k++;
+		}
+	}
+}
 }
---
+++ Day6/src/com/demo/test/TestLinkedList.java
@@ -0,0 +1,21 @@
+package com.demo.test;
+import com.demo.service.*;
+public class TestLinkedList {
+
+	public static void main(String[] args) {
+		SinglyLinkedList list = new SinglyLinkedList();
+		list.addNode(23);
+		list.addNode(59);
+		list.addNode(5);
+		list.addNode(18);
+		list.displayNode();
+		list.addByPosition(1, 58);
+		System.out.println();
+		list.displayNode();
+		list.addByNum(5, 5);
+		System.out.println();
+		list.displayNode();
+	}
+
+}
+ 

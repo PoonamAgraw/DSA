27b8f8415329dc8f8a3d82475ed92b79ed5a4186
mukund <autimukund9@gmail.com>
2025-11-06 16:06:46 +0530

    dsa

--- Day8/src/com/demo/test/TestCircularLinkedList.java
+++ Day8/src/com/demo/test/TestCircularLinkedList.java
@@ -1,10 +1,23 @@
 package com.demo.test;
-
+import com.demo.service.*;
 public class TestCircularLinkedList {
 
 	public static void main(String[] args) {
-	
-
+	CircularLinkedList clist = new CircularLinkedList();
+    clist.addNode(67);
+    clist.displayData();
+    clist.addNode(12);
+    clist.displayData();
+    clist.addNode(8);
+    clist.displayData();
+    clist.addByPosition(3, 50);
+    clist.displayData();
+    clist.addByValue(54);
+    clist.displayData();
+    clist.deleteByposition(2);
+    clist.displayData();
+    clist.deleteByValue(8);
+    clist.displayData();
 	}
 
 }
